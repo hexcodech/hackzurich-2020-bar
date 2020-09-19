fcf154e70c5536569425a13832b06bd09cd26099
Marc Berchtold <me@echolot.io>
2020-09-19 11:39:01 +0200

    Eaternity service

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,7 +1,10 @@
 import { Controller, Get } from '@nestjs/common';
+import { EaternityService } from './eaternity.service';
 
 @Controller()
 export class AppController {
+  constructor(private eaternityService: EaternityService) {}
+
   @Get()
   getHello(): {message: string} {
     return { message: "👀" };
--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,13 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
+import { EaternityService } from './eaternity.service';
 
 @Module({
   imports: [
     MongooseModule.forRoot(process.env.MONGODB_URI),
   ],
   controllers: [AppController],
-  providers: [],
+  providers: [EaternityService],
 })
 export class AppModule {}
