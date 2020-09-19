231b43bbe031a9499fcbe279fa23659d2698cc64
Marc Rettenbacher <tyratox.rettenbacher@gmail.com>
2020-09-19 13:17:52 +0200

    added migros.service

--- src/migros.service.ts
+++ src/migros.service.ts
@@ -1,48 +1,31 @@
 import { Injectable, HttpException } from "@nestjs/common";
 import fetch from "node-fetch";
 
-export interface IngredientDescription {
-  name: string;
-  origin: string;
-  transport?: "air" | "ground";
-  production?: "standard" | "greenhouse" | "organic" | "fair-trade" | "farm" /* fishes & game only */ | "wild-caught" /* fishes & game only */;
-  conservation?: "fresh" | "frozen" | "dried" | "conserved" | "canned" | "boiled-down";
-  packaging?: "none" | "plastic" | "paper" | "pet" | "tin" | "alu" | "glas" | "cardboard" | "tetra";
+export interface ProductDescription {
+  id: number;
 }
 
-export interface IngredientInfo {
-  "co2-value": number;
-  rating: string;
-  "food-unit": number;
+export interface ProductInfo {
+  productId: number,
+  productName: string,
+  productCategoriesCode: string,
+  productImage_transparentOriginal: string,
+  productPriceItemPrice: number,
+  productOriginProducing_country?: string,
+  productOriginCountry_of_Origin?: string
 }
 
 @Injectable()
-export class EaternityService {
-  async getIngredientInfo(description: IngredientDescription): Promise<IngredientInfo> {
-    const date = new Date();
+export class MigrosService {
+  async getProductInfo(description: ProductDescription): Promise<ProductInfo> {
     let res;
     try {
-      res = await fetch(`${process.env.EATERNITY_BASE_URL}/api/kitchens/${process.env.EATERNITY_KITCHEN_ID}/recipes/${process.env.EATERNITY_RECIPE_ID}?transient=true`, {
-        method: 'PUT',
+      res = await fetch(`${process.env.MIGROS_BASE_URL}/products?search=${description.id}`, {
+        method: 'GET',
         headers: {
           "Content-Type": "application/json",
-          "Authorization": `Basic ${Buffer.from(process.env.EATERNITY_API_KEY).toString('base64')}`
-        },
-        body: JSON.stringify({
-          "recipe": {
-            "location": process.env.EATERNITY_LOCATION,
-            "date": `${date.getFullYear()}-${(date.getMonth()+1).toString().padStart(2, '0')}-${date.getDate().toString().padStart(2, '0')}`,
-            "ingredients": [
-              {
-                id: "hack",
-                names: [{language: "de", value: description.name}],
-                amount: 1000,
-                unit: "gram",
-                ...description
-              }
-            ]
-          }
-        })
+          "Authorization": `Basic ${Buffer.from(process.env.MIGROS_API_KEY).toString('base64')}`
+        }
       });
     } catch (err) {
       throw new HttpException(err.toString(), 500);
