969641c7d7ab2848780f0606eaa57e90f2b84546
Marc Berchtold <me@echolot.io>
2020-09-20 12:58:18 +0200

    Static file hosting

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,10 +6,15 @@ import { PurchaseModule } from './purchase/purchase.module';
 import { GeographicAreaModule } from './geographic-area/geographic-area.module';
 import { AlternativeProductModule } from './alternative-product/alternative-product.module';
 import { EaternityModule } from './eaternity/eaternity.module';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { join } from 'path';
 
 @Module({
   imports: [
     MongooseModule.forRoot(process.env.MONGODB_URI),
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '..', 'public'),
+    }),
     UserModule,
     PurchaseModule,
     GeographicAreaModule,
